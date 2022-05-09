e6cd134b64812b1423870be678e96a27196c5c5d
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-05-09 18:10:31 +0600

    Adding New User page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './app.scss';
 import UserList from "./pages/UserList/UserList";
 import User from "./pages/User/User";
+import NewUser from "./pages/NewUser/NewUser";
 
 function App() {
   return (
@@ -22,9 +23,9 @@ function App() {
                   <Route path="/user/:userId">
                       <User />
                   </Route>
-                  {/*<Route path="/newUser">*/}
-                  {/*    <NewUser />*/}
-                  {/*</Route>*/}
+                  <Route path="/newUser">
+                      <NewUser />
+                  </Route>
                   {/*<Route path="/products">*/}
                   {/*    <ProductList />*/}
                   {/*</Route>*/}
