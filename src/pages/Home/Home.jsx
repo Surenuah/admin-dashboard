da6735df537348d9e47c56530f24cfddfe972fc3
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-05-09 16:55:10 +0600

    Adding widgets

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,18 @@ import './home.scss';
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
 import Chart from "../../components/Chart/Chart";
 import { userData } from "../../data";
+import WidgetSm from "../../components/WidgetSm/WidgetSm";
+import WidgetLg from "../../components/WidgetLg/WidgetLg";
 
 function Home() {
     return (
         <div className='home'>
             <FeaturedInfo />
             <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <div className='home-widgets'>
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
